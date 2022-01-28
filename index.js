6f36d8a49fe5ea647dc05eecfc61dc621200efd8
YOVKHYMYSHCH nazar <nazar.yovkh@gmail.com>
2022-01-28 16:31:32 +0100

    up

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  const arrow = document.getElementById("img");
+  arrow.style.animation = 'step'
+  arrow.style.animationDuration = '3s'
+  arrow.style.animationIterationCount = 'infinite'
+  arrow.style.animationTimingFunction = 'linear'
+});
