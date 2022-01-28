6aadd7afbf4f8be7faaae72389bc4c67608f6f22
YOVKHYMYSHCH nazar <nazar.yovkh@gmail.com>
2022-01-28 17:31:55 +0100

    up

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  const arrow = document.getElementById("img");
-  arrow.style.animation = 'step'
-  arrow.style.animationDuration = '3s'
-  arrow.style.animationIterationCount = 'infinite'
-  arrow.style.animationTimingFunction = 'linear'
+  const head = document.getElementById("head");
+  head.style.animation = "face";
+  head.style.animationDuration = "10s";
+  head.style.animationIterationCount = "infinite";
+  head.style.animationTimingFunction = "linear";
+
+  const arrow = document.getElementById("arrow");
+  arrow.style.animation = "stepsH";
+  arrow.style.animationDuration = "3s";
+  arrow.style.animationIterationCount = "infinite";
+  arrow.style.animationTimingFunction = "linear";
+
+  arrow.addEventListener(
+    "click",
+    function () {
+      console.log("BITE")
+      document.getElementById('door').style.backgroundImage = "url('./assets/door-opened.png')"
+    },
+    { once: true }
+  );
 });
